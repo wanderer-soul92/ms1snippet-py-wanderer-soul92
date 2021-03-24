a6dc765704768aa686b202310d5afedf619a2f32
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-03-24 23:38:05 +0530

    Add files via upload

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,19 +1,27 @@
 
-def  _give_me_a_good_name(value, nextValue, maxDelta):
+def  in_permissible_range(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
 
-def validate_soc_reading(values):
+def validate_reading(value,parameter):
+    if value == None:
+        return print("No Value given as input")
+    else:
+        validate_Para_reading(value,parameter)
+
+        
+def validate_Para_reading(values,parameter):
+  threshold = { 'soc': 0.05, 'current': 0.1}
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
+    if(not in_permissible_range(values[i], values[i + 1], threshold[parameter])):
       return False
   return True
 
-def validate_current_reading(values):
+"""def validate_current_reading(values):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
     if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
       return False
-  return True
+  return True """
--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -4,12 +4,17 @@ import sensor_validate
 class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soc_jumps(self):
     self.assertFalse(
-      sensor_validate.validate_soc_reading([0.0, 0.01, 0.5, 0.51])
+      sensor_validate.validate_reading([0.0, 0.01, 0.5, 0.51], 'soc')
     )
   
   def test_reports_error_when_current_jumps(self):
     self.assertFalse(
-      sensor_validate.validate_current_reading([0.03, 0.03, 0.03, 0.33])
+      sensor_validate.validate_reading([0.03, 0.03, 0.03, 0.33], 'current')
+    )
+  
+  def test_reports_error_when_current_jumps(self):
+    self.assertFalse(
+     sensor_validate.validate_reading(None, 'current')
     )
 
 if __name__ == "__main__":
